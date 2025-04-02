96a041e4a0c56625dc2f2f5ad77034e8247d6fee
CamilaFre <camila.frech@hotmail.com>
2025-04-01 21:17:53 -0300

    adicionando o x

--- desafio_show.py
+++ desafio_show.py
@@ -2,45 +2,62 @@
 quiz = [
 
     { "pergunta": "Qual a capital do Brasil?",
-      "alternativas": ["Sao Paulo", "Brasília", "Rio de Janeiro"],
+      "alternativas": ["Sao Paulo", "Brasília", "Rio de Janeiro","Resende"],
       "correta": "Brasília"
 
     },
 
     {"pergunta": "Qual a capital da Alemanha?",
-     "alternativas": ["Berlim", "Atenas", "Munique"],
+     "alternativas": ["Berlim", "Atenas", "Munique","Bayern"],
      "correta": "Berlim"
 
      },
 
     {"pergunta": "Qual o Deus do Submundo?",
-     "alternativas": ["Zeus", "Hades", "Poseidon"],
+     "alternativas": ["Zeus", "Hades", "Poseidon","Persephone"],
      "correta": "Hades"
 
+     },
+
+    {"pergunta": "Qual é o nome do irmão do Dexter?",
+     "alternativas": ["Joey", "Harrison", "Harry","Brian"],
+     "correta": "Brian"
+
      }
+
 ]
 
-l = len(quiz)
+l = len(quiz[0]['alternativas'])
+y = len(quiz)
 
 print(l)
 
 i = 0
 c = 0
 
-while i < l:
+while i < y:
 
     print(quiz[i]['pergunta'])
+    print()
     for n, valor in enumerate(quiz[i]['alternativas']):
-     
         print(n + 1, "-", valor)
-    r = input( )
-    if r.isdecimal() and int(r) <= 3:
+        print()
+    r = input("Digite o número da alternativa correta (ou 'X' para sair):" )
+    print()
+
+    if r.lower() =="X".lower():
+        break
+
+    if r.isdecimal() and int(r) <= l:
         r = int(r)
         if (quiz[i]["alternativas"][r - 1]) == quiz[i]["correta"]:
             print("Você acertou, miserávi!!!")
+            print()
             c +=1
         else:
             print("Você errou feio, errou rude!!!")
+            print(f"A resposta correta é {quiz[i]["correta"]}")
+            print()
     else:
         print("você digitou um valor inválido, tente novamente")
         continue
@@ -48,4 +65,5 @@ while i < l:
     i = i+1
 
 taxa = c/l
+print()
 print(f'Você acertou {c} perguntas de {l} o que equivale a uma taxa de acertos de {taxa:.0%}  ')
